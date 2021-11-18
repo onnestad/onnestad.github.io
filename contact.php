8e9a6e5557fd35680c32769f46335e9c93f6e048
Leonard Ilanius <leonard.ilanius@onnestadsfolkhogskola.se>
2021-11-18 14:42:55 +0100

    visual studio

--- contact.php
+++ contact.php
@@ -55,18 +55,7 @@
 		</div>
 	</section>
 
-	<footer>
-		<h1 class="gridHead"> The Circuit </h1>
-		<nav class="friendLink">
-			<span><a href="https://aentity66.github.io"> Aentity66 </a> </span>
-			<span><a href="https://tobiaswijk.github.io/"> Tobias </a> </span>
-			<span><a href="https://onnell.github.io"> Lanelle </a> </span>
-			<span><a href="https://mattejo.github.io"> Jonas </a> </span>
-			<span><a href="https://peghmo.github.io"> Alexander </a> </span>
-			<span><a href="https://onnestad.github.io"> Onnestad IT </a> </span>
-		</nav>
-	</footer>
-
+	<?php include "footer.php"; ?>
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-0T2CGJJ1WQ"></script>
---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer>
+  <h1 class="gridHead"> The Circuit </h1>
+  <nav class="friendLink">
+    <span><a href="https://aentity66.github.io"> Aentity66 </a> </span>
+    <span><a href="https://tobiaswijk.github.io/"> Tobias </a> </span>
+    <span><a href="https://onnell.github.io"> Lanelle </a> </span>
+    <span><a href="https://mattejo.github.io"> Jonas </a> </span>
+    <span><a href="https://peghmo.github.io"> Alexander </a> </span>
+    <span><a href="https://onnestad.github.io"> Onnestad IT </a> </span>
+  </nav>
+</footer>
